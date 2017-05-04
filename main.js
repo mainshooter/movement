4a2d2ecb4de6aa1dc742981eabeb358245003644
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-04 09:41:06 +0200

    Shorter the syntax

--- main.js
+++ main.js
@@ -6,16 +6,15 @@ var circle = document.getElementById('circle');
 var circleWith = 20;
 // To set the mouse the center of the circle
 
-
 function mouseMove(event) {
   // Moves the mouse
-  document.getElementById('circle').style.top = (event.pageY - circleWith) + "px";
-  document.getElementById('circle').style.left = (event.pageX - circleWith) + "px";
+  console.log("RUN");
+  circle.style.top = (event.pageY - circleWith) + "px";
+  circle.style.left = (event.pageX - circleWith) + "px";
 }
 var timesClicked = 0;
 // When timesclicked by circle change
 function circleStyleChange(event) {
-  console.log("RUN");
   // This function changes the circle style when there is 2 times clickked
   if (timesClicked === 0) {
     // Change style to other style
